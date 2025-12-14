e973c08196b1131f48efe9a3025b24096ae56e89
Salun660610850 <sarankaewtemtam@gmail.com>
2025-12-14 11:27:36 +0700

    lab

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,18 +1,62 @@
-//Dialog of the first example is given below.
+#include <iostream>
+#include <string>
+#include <cmath> 
 
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
-Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
-Fahsai: Let's go to the cinema together!!!
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
-*/
+using namespace std;
+
+int main() {
+
+    string name;
+    string student_id_str;
+    string movie;
+    string day;
+    string final_quote;
+
+
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+    cout << "?????: ";
+
+    getline(cin, name);
+
+    cout << "Fahsai: Wow!!! " << name << " is a really cool name.\n";
+
+
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+    cout << name << ": ";
+    getline(cin, student_id_str);
+
+
+    if (student_id_str.length() >= 2) {
+        try {
+            int two_digit_prefix = stoi(student_id_str.substr(0, 2));
+            int gear_number = two_digit_prefix - 12;
+
+            cout << "Fahsai: I think you may be GEAR " << gear_number << ". I have a free movie ticket for you.\n";
+            cout << "Fahsai: Let's go to the cinema together!!!\n";
+
+
+            cout << "Fahsai: What movie do you want to watch?\n";
+            cout << name << ": ";
+            getline(cin, movie);
+
+
+            cout << "Fahsai: So....which day are you free to go with me?\n";
+            cout << name << ": ";
+            getline(cin, day);
+
+            cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << movie << " with you.\n";
+
+            cout << name << ": ";
+            getline(cin, final_quote);
+
+            cout << "Fahsai: 555+ see you " << day << ". Bye Bye << "\" << "(^ ^)/\n";
+
+        } catch (const std::exception& e) {
+            cout << "Fahsai: Invalid student ID format. Let's just talk about movies.\n";
+        }
+    } else {
+        cout << "Fahsai: That ID seems too short. Let's just talk about movies.\n";
+    }
+
+    return 0;
+}
